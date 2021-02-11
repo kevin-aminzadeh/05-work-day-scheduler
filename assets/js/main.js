22cffbea337488a72a369f3069218ba2d762724a
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-02-11 17:33:08 +1030

    feat: Implement render logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,8 +24,127 @@ function getOrdinalIndicator(num) {
   }
 }
 
+// Render current day in header
 $("#currentDay").text(
   `${now.weekdayLong}, ${now.monthLong} ${now.day}${getOrdinalIndicator(
     now.day
   )} `
 );
+
+// Handle Local Storage Operations
+class AppStorageService {
+  constructor() {}
+
+  onInit() {}
+
+  onUpdate() {}
+
+  getEvents() {
+    const events = JSON.parse(localStorage.getItem("events"));
+    return events;
+  }
+
+  setEvent(hour, text) {
+    let events = [];
+    const event = {
+      hour: hour,
+      text: text,
+    };
+
+    if (JSON.parse(localStorage.getItem("events"))) {
+      events = JSON.parse(localStorage.getItem("events"));
+    }
+    events.push(event);
+    localStorage.setItem("events", JSON.stringify(events));
+  }
+}
+
+// Time Block Class Definition
+class TimeBlock {
+  constructor(hour, blockState, scheduledEvent, rootEl, id) {
+    this.id = id;
+    this.rootEl = rootEl;
+    this.hour = hour;
+    this.blockState = blockState;
+    this.scheduledEvent = scheduledEvent;
+    this.template = `
+      <div class="row time-block" id="timeBlock-${this.id}">
+        <div class="col-3 col-md-2 col-lg-1 hour">
+          <p>${this.hour}</p>
+        </div>
+
+        <textarea class="col ${this.blockState}">Event</textarea>
+
+        <button
+          class="btn saveBtn col-3 col-md-2 col-lg-1 d-flex align-items-center justify-content-center"
+          id="${this.id}-btn"
+        >
+          <i class="fas fa-save fa-lg"></i>
+        </button>
+      </div>
+    `;
+  }
+
+  updateState(state) {}
+
+  render() {
+    return this.template;
+  }
+
+  attachEventHandlers() {
+    $(`#${this.id}-btn`).click(() => {
+      console.log(`#${this.id}-btn`);
+    });
+  }
+
+  addEvent(descrp) {}
+}
+
+// Application Controller Class Definition
+class App {
+  constructor() {
+    this.timeBlockWrapper = $("#time-block-wrapper");
+    this.appStorageService = new AppStorageService();
+    this.savedEvents;
+    this.timeBlocks = [];
+    this.dayStartHour = 9;
+    this.dayEndHour = 17;
+  }
+
+  init() {
+    this.savedEvents = this.appStorageService.getEvents();
+    console.log(this.timeBlockWrapper);
+    this.render();
+  }
+
+  update() {}
+
+  saveEvent() {}
+
+  render() {
+    for (let i = this.dayStartHour; i <= this.dayEndHour; i++) {
+      let blockState;
+
+      if (i < now.hour) {
+        blockState = "past";
+      } else if (i === now.hour) {
+        blockState = "present";
+      } else if (i > now.hour) {
+        blockState = "future";
+      }
+      const timeBlock = new TimeBlock(
+        i,
+        blockState,
+        undefined,
+        this.timeBlockWrapper,
+        i
+      );
+      $(this.timeBlockWrapper).append(timeBlock.render());
+      timeBlock.attachEventHandlers();
+    }
+  }
+}
+
+let app = new App();
+
+app.init();
