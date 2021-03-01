839f668d7d92ad559d58c19303d4ee61c4ee5216
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-03-01 17:14:47 +1030

    feat: UI data update mechanism

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
-var DateTime = luxon.DateTime;
+const DateTime = luxon.DateTime;
 
+// Get Current Date and Time
 var now = DateTime.local();
 
-// Determine the correct Ordinal Indicator (i.e. "st", "rd" or "th") for given day of the month
+// Determine the correct Ordinal Indicator (i.e. "st", "rd" or "th") for given day of the month and return it
 function getOrdinalIndicator(num) {
   const suffix = ["th", "st", "nd", "rd"];
   let lastDigit = num.toString().slice(-1);
@@ -12,6 +13,7 @@ function getOrdinalIndicator(num) {
     return suffix[0];
   }
 
+  // Handle common cases
   switch (lastDigit) {
     case "1":
       return suffix[1];
@@ -33,29 +35,61 @@ $("#currentDay").text(
 
 // Handle Local Storage Operations
 class AppStorageService {
-  constructor() {}
+  constructor() {
+    this.events = [];
+  }
 
-  onInit() {}
+  init() {
+    console.log(`Initializing Storage Service...\n`);
+    if (
+      localStorage.getItem("events") &&
+      JSON.parse(localStorage.getItem("events")).length
+    ) {
+      this.events = JSON.parse(localStorage.getItem("events"));
+    }
+  }
 
-  onUpdate() {}
-
+  // Get ALL Saved Events
   getEvents() {
-    const events = JSON.parse(localStorage.getItem("events"));
-    return events;
+    console.log("Fetching Events...");
+    return this.events;
+  }
+
+  // Get individual saved event by hour (24HR format)
+  getEvent(hour) {
+    if (this.events.length) {
+      let index = this.events.findIndex((event) => {
+        return event.hour == hour;
+      });
+
+      return index;
+    } else {
+      return false;
+    }
   }
 
   setEvent(hour, text) {
-    let events = [];
     const event = {
       hour: hour,
       text: text,
     };
+    // If an event with matching hour value exists, store its index (matchingEventIndex === -1 if no match is found)
+    const matchingEventIndex = this.events.findIndex((event) => {
+      return event.hour === hour;
+    });
 
-    if (JSON.parse(localStorage.getItem("events"))) {
-      events = JSON.parse(localStorage.getItem("events"));
+    // If no event with matching hour value exists, push event object to events array
+    if (matchingEventIndex == -1) {
+      this.events.push(event);
     }
-    events.push(event);
-    localStorage.setItem("events", JSON.stringify(events));
+    // If an event with a matching hour value DOES exist, update the value of its text property to the value of the text parameter
+    else {
+      this.events[matchingEventIndex].text = text;
+    }
+
+    // Stringify this.events array and set it to local storage;
+    localStorage.setItem("events", JSON.stringify(this.events));
+    console.log(`Event Saved! "${text}" at ${hour}:00.`);
   }
 }
 
@@ -68,38 +102,40 @@ class TimeBlock {
     this.hour = hour;
     this.blockState = blockState;
     this.scheduledEvent = scheduledEvent;
-    this.template = `
-      <div class="row time-block" id="timeBlock-${this.id}">
-        <div class="col-3 col-md-2 col-lg-1 hour">
-          <p>${this.hour}:00</p>
-        </div>
-
-        <textarea class="col ${this.blockState}" id="${this.id}-text">Event</textarea>
-
-        <button
-          class="btn saveBtn col-3 col-md-2 col-lg-1 d-flex align-items-center justify-content-center"
-          id="${this.id}-btn"
-        >
-          <i class="fas fa-save fa-lg"></i>
-        </button>
-      </div>
-    `;
+    this.template;
   }
 
-  updateState(state) {}
+  updateText(text) {
+    this.scheduledEvent = text;
+  }
 
   render() {
+    this.template = `
+      <div class="time-block">
+        <div class="row" id="timeBlock-${this.id}">
+          <div class="col-3 col-md-2 col-lg-1 hour">
+            <p>${this.hour}:00</p>
+          </div>
+
+          <textarea class="col ${this.blockState}" id="${this.id}-text">${this.scheduledEvent}</textarea>
+
+          <button
+            class="btn saveBtn col-2 col-md-2 col-lg-1 d-flex align-items-center justify-content-center"
+            id="${this.id}-btn"
+          >
+            <i class="fas fa-save fa-lg"></i>
+          </button>
+        </div>
+      </div>
+    `;
     return this.template;
   }
 
   attachEventHandlers() {
     $(`#${this.id}-btn`).click(() => {
-      console.log(`#${this.id}-btn`);
       app.saveEvent(this.hour, $(`#${this.id}-text`).val());
     });
   }
-
-  addEvent(descrp) {}
 }
 
 // Application Controller Class Definition
@@ -114,18 +150,16 @@ class App {
   }
 
   init() {
+    console.log(`Initializing Application...\n`);
+    this.appStorageService.init();
     this.savedEvents = this.appStorageService.getEvents();
-    console.log(this.timeBlockWrapper);
+    console.log(`Rendering Application UI...\n`);
+
+    this.initTimeBlocks();
     this.render();
   }
 
-  update() {}
-
-  saveEvent(eventID, text) {
-    this.appStorageService.setEvent(eventID, text);
-  }
-
-  render() {
+  initTimeBlocks() {
     for (let i = this.dayStartHour; i <= this.dayEndHour; i++) {
       let blockState;
 
@@ -136,14 +170,38 @@ class App {
       } else if (i > now.hour) {
         blockState = "future";
       }
-      const timeBlock = new TimeBlock(
+
+      let timeBlock = new TimeBlock(
         i,
         blockState,
-        undefined,
+        "Event",
         this.timeBlockWrapper,
         i,
         this
       );
+
+      this.timeBlocks.push(timeBlock);
+    }
+  }
+
+  // Update text in time blocks to reflect saved event data
+  updateTimeBlocks() {
+    for (const event of this.savedEvents) {
+      for (const timeBlock of this.timeBlocks) {
+        if (event.hour === timeBlock.hour) {
+          timeBlock.updateText(event.text);
+        }
+      }
+    }
+  }
+
+  saveEvent(eventID, text) {
+    this.appStorageService.setEvent(eventID, text);
+  }
+
+  render() {
+    this.updateTimeBlocks();
+    for (const timeBlock of this.timeBlocks) {
       $(this.timeBlockWrapper).append(timeBlock.render());
       timeBlock.attachEventHandlers();
     }
